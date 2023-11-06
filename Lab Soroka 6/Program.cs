5343b51f2a4d9fd1a9fc9cd0a8d41d96d15cdf89
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-11-06 18:28:52 +0200

    Добавьте файлы проекта.

---
+++ Lab Soroka 6/Program.cs
@@ -0,0 +1,29 @@
+﻿//Вариант 4
+//try
+//{
+//Console.Write(" Введите x: ");
+//double x = double.Parse(Console.ReadLine());
+
+//Console.Write(" Введите n: ");
+//int n = int.Parse(Console.ReadLine());
+
+//double S = 0;
+
+//for (int i = 1; i <= n; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        S -= Math.Sin(i * x) / i;
+//    }
+//    else
+//    {
+//        S += Math.Sin(i * x) / i;
+//    }
+//}
+//Console.WriteLine($"S={S:F2}");
+
+//}
+//catch (Exception err)
+//{
+//    Console.WriteLine(err);
+//}
